2afcc34fab51a833cd5bf6e5d69e5c24db701cd5
Gabippeira <Gp18997543977@gmail.com>
2023-02-28 14:31:52 +0000

    salvar

---
+++ NomeIdade.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class NomeIdade {
+    
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        String Nome;
+}
---
+++ NumeroInteiro.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class NumeroInteiro {
+
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+        int numero;
+
+        System.out.println("Informe o numero ");
+        numero = input.nextInt();
+
+        if (numero > 0 )
+        {
+            System.out.print("o numero é positivo \n");
+
+        } else if (numero < 0) {
+            System.out.print("O numero é negativo \n");
+
+        } else if (numero == 0) {
+            System.out.print("O numero é 0 \n");
+
+       }
+    }
+}
