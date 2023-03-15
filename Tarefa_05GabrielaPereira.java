f730c8e3aeeb5423c10d0f95a34e10e7a0fa0a8d
Gabippeira <Gp18997543977@gmail.com>
2023-03-15 17:08:02 +0000

    salvar

--- Tarefa_05GabrielaPereira.java
+++ Tarefa_05GabrielaPereira.java
@@ -1,17 +1,39 @@
 import java.util.Scanner;
 public class Tarefa_05GabrielaPereira {
-    
+
     public static void main(String[] args){
 
-        Scanner input = new Scanner(System.in);
-        int semana;
+    Scanner input = new Scanner(System.in);
+    double salario1, SalarioNovo;
+    
+    System.out.println("Informe o valor do salario atual: ");
+    salario1 = input.nextFloat();
 
-        System.out.println("Informe um numero correspondente a um dia de semana:");
-        semana = input.nextInt();
+    if (salario1 >= 1000 && salario1 <= 2000)
+    {
+        SalarioNovo = salario1 * 1.15;
+        System.out.print("Aqui esta seu salario novo: " + SalarioNovo + "\n");
+    }
 
-        if (semana == 1)
-        {
-            System.out.println("");
-        }
-       }
+    else if (salario1 >= 2000 && salario1 <= 3000)
+    {
+        SalarioNovo = salario1 * 1.10;
+        System.out.print("Aqui esta seu salario novo: " + SalarioNovo + "\n");
+    }
+
+    else if(salario1> 3000)
+    {
+        SalarioNovo = salario1 * 1.5;
+        System.out.print("Aqui esta seu salario novo: " +SalarioNovo + "\n");
+    }
+
+    else if(salario1 < 1000)
+    {
+        System.out.print("Você é estagiário, não reclame do salário. \n");
+    }
+
+
+    }
+
+    
 }
